8f1960a6160b225fc8b44ca51331dc3dada1eb1b
Kamezaki-jp <champagne.es335@gmail.com>
2021-02-06 07:36:19 +0000

    [Add] book_edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1ba77ba3f5c2f600cd6c86c8137f09a3832a26cbdebdaa9257e345c74e14b0600dbc4c78d63eabefcbac386dd690eb82e30a1646f911bcddc84d49df7ec4ae67'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about'
+
+  resources :books
+  resources :users
+  
 end
